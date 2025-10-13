e86e1a81b308652d2bd208d58392121eb46c0450
Jason64364 <jason@newbitsnow.com>
2025-10-13 09:19:08 -0700

    JN

--- imports.tf
+++ imports.tf
@@ -4,14 +4,14 @@ import {
   id = "arn:aws:secretsmanager:us-west-2:438465144115:secret:shopify/admin-xlmuwX"
 }
 
-# 2) IAM role
+# IAM role
 import {
   to = aws_iam_role.lambda_role
-  id = "arn:aws:iam::438465144115:role/shopify-mcp-lambda-role"
+  id = "shopify-mcp-lambda-role"
 }
 
-# 3) CloudWatch log group
+# CloudWatch log group
 import {
   to = aws_cloudwatch_log_group.lambda_logs
-  id = "arn:aws:logs:us-west-2:438465144115:log-group:/aws/lambda/shopify-mcp-mcp-shopify-admin:*"
+  id = "/aws/lambda/shopify-mcp-mcp-shopify-admin"
 }
